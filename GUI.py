5293881b889ea81ef2b94413697c4a5b4ac45b24
Ahmad Zaki <107484564+Ahmad-Zaki05@users.noreply.github.com>
2025-01-29 10:29:51 +0200

    Added ThrustersWidget Class (Incomplete)
    The code works fine when it is standalone, but it doesn't work correctly when integrated with the rest of the code

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 import sys, cv2, random
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QGridLayout, QVBoxLayout
-from PyQt5.QtGui import QImage, QPixmap, QFont
-from PyQt5.QtCore import QThread, pyqtSignal, QTimer
+from PyQt5.QtGui import QImage, QPixmap, QFont, QPainter, QColor, QPen, QBrush
+from PyQt5.QtCore import QThread, pyqtSignal, QTimer, Qt
 
 class CameraWidget (QLabel) :
     def __init__(self, parent, cam, w, h):
@@ -63,6 +63,116 @@ class OrientationsWidget (QWidget) :
         self.pitchLabel.setText ("Pitch: " + str (pitchReading))
         self.rollLabel.setText ("Roll: " + str (rollReading))
 
+class ThrustersWidget (QWidget) :
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.parent = parent
+
+        self.setMinimumSize (parent.width () // 3, parent.height () // 4)
+        
+        # Default colors
+        self.square_color = QColor(0, 0, 0)  # Black for center square
+
+        self.circle_colors = [
+            QColor(0, 255, 0),  # Top
+            QColor(0, 255, 0)
+            # ,  # Bottom
+            # # QColor(0, 255, 0),  # Left
+            # QColor(0, 255, 0)   # Right
+        ]
+
+        self.rotated_square_colors = [
+            QColor(0, 255, 0),  # Front-left
+            QColor(0, 255, 0),  # Front-right
+            QColor(0, 255, 0),  # Back-left
+            QColor(0, 255, 0)   # Back-right
+        ]
+
+    def set_colors(self):
+        """Allows changing colors dynamically"""
+
+        frontRightSpeed = random.randint (0, 255)
+        backRightSpeed = random.randint (0, 255)
+        frontLeftSpeed = random.randint (0, 255)
+        backLeftSpeed = random.randint (0, 255)
+        upFrontSpeed = random.randint (0, 255)
+        upBackSpeed = random.randint (0, 255)
+
+        self.square_color = QColor(0, 0, 0)
+        self.circle_colors = [
+            QColor(upFrontSpeed, 255 - upFrontSpeed, 0),  # Top
+            QColor(upBackSpeed, 255 - upBackSpeed, 0)
+            # ,  # Bottom
+            # QColor(0, 255, 0),  # Left
+            # QColor(0, 255, 0)   # Right
+        ]
+        self.rotated_square_colors = [
+            QColor(frontLeftSpeed, 255 - frontLeftSpeed, 0),  # Front-left
+            QColor(frontRightSpeed, 255 - frontRightSpeed, 0),  # Front-right
+            QColor(backLeftSpeed, 255 - backLeftSpeed, 0),  # Back-left
+            QColor(backRightSpeed, 255 - backRightSpeed, 0)   # Back-right
+        ]
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+
+        # Get parent size constraints
+        max_width = self.parent.width() // 3
+        max_height = self.parent.height() // 4
+        size = min(max_width, max_height)
+
+        # Central square (50% of size)
+        square_size = int(size * 0.5)
+        square_x = (self.width() - square_size) // 2
+        square_y = (self.height() - square_size) // 2
+
+        # Draw the black center square
+        painter.setPen(QPen(self.square_color, 3))
+        painter.setBrush(QBrush(Qt.NoBrush))
+        painter.drawRect(square_x, square_y, square_size, square_size)
+
+        # Circles' and Rotated Squares' Positions
+        offsets = [
+            (square_x + square_size // 2, square_y - square_size // 3),  # Top
+            (square_x + square_size // 2, square_y + square_size + square_size // 3)
+            # ,  # Bottom
+            # (square_x - square_size // 3, square_y + square_size // 2),  # Left
+            # (square_x + square_size + square_size // 3, square_y + square_size // 2)  # Right
+        ]
+
+        rotated_offsets = [
+            (square_x - square_size // 3, square_y - square_size // 3),  # Top-left
+            (square_x + square_size + square_size // 6, square_y - square_size // 3),  # Top-right
+            (square_x - square_size // 3, square_y + square_size + square_size // 6),  # Bottom-left
+            (square_x + square_size + square_size // 6, square_y + square_size + square_size // 6)  # Bottom-right
+        ]
+
+        circle_radius = int(square_size * 0.2)
+
+        # Draw Circles with individual colors
+        for i, (x, y) in enumerate(offsets):
+            painter.setBrush(QBrush(self.circle_colors[i]))
+            painter.drawEllipse(x - circle_radius, y - circle_radius, circle_radius * 2, circle_radius * 2)
+
+        # Draw Rotated Squares with individual colors
+        for i, (x, y) in enumerate(rotated_offsets):
+            painter.setBrush(QBrush(self.rotated_square_colors[i]))
+            painter.save()
+            painter.translate(x, y)
+            painter.rotate(45)  # Rotate by 45 degrees
+            painter.drawRect(-circle_radius, -circle_radius, circle_radius * 2, circle_radius * 2)
+            painter.restore()
+
+    def update (self) :
+        self.set_colors ()
+        self.paintEvent (None)
+
+
+        
+
+
+
 class MainWindow (QMainWindow) :
     def __init__(self) :
         super().__init__()
@@ -86,20 +196,21 @@ class MainWindow (QMainWindow) :
         self.rightCameraWidget = CameraWidget (self, 2, self.width () // 3, self.height () // 4)
         self.orientationsWidget = OrientationsWidget (self)
         self.controllerWidget = QWidget ()
-        self.thrustersWidget = QWidget ()
+        self.thrustersWidget = ThrustersWidget (self)
         self.tasksWidget = QWidget ()
         self.scriptsWidget = QWidget ()
         
         grid = QGridLayout ()
 
-        grid.setColumnMinimumWidth (self.width () // 3, 0)
-        grid.setColumnMinimumWidth (self.width () // 3, 1)
-        grid.setColumnMinimumWidth (self.width () // 3, 2)
+        grid.setColumnMinimumWidth(0, self.width() // 3)
+        grid.setColumnMinimumWidth(1, self.width() // 3)
+        grid.setColumnMinimumWidth(2, self.width() // 3)
 
-        grid.setRowMinimumHeight (self.height () // 4, 0)
-        grid.setRowMinimumHeight (self.height () // 4, 1)
-        grid.setRowMinimumHeight (self.height () // 4, 2)
-        grid.setRowMinimumHeight (self.height () // 4, 3)
+        grid.setRowMinimumHeight(0, self.height() // 4)
+        grid.setRowMinimumHeight(1, self.height() // 4)
+        grid.setRowMinimumHeight(2, self.height() // 4)
+        grid.setRowMinimumHeight(3, self.height() // 4)
+
 
 
         grid.addWidget (self.leftCameraWidget, 0, 0, 2, 1)
