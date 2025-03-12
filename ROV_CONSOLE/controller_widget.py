917287cdaf73157edb61bc619ed4d4a47c0ba6e9
Youssef Samy <office.youssefkassem@gmail.com>
2025-03-12 22:54:50 +0200

    sped up updates

--- ROV_CONSOLE/controller_widget.py
+++ ROV_CONSOLE/controller_widget.py
@@ -13,14 +13,14 @@ class ControllerDisplay(QWidget):
         self.circle.setIcon(QIcon(f'{icons_path}/Button - PS Circle White 1.png'))
         self.circle.setIconSize(QSize(60, 60))  # Set the icon size
         self.circle.setStyleSheet("border: none;")  # Remove the button border
-        self.circle.move(420, 155)
+        self.circle.move(410, 155)
 
         self.cross = QPushButton(self)
         self.cross.setObjectName("cross")
         self.cross.setIcon(QIcon(f'{icons_path}/Button - PS Cross White 1.png'))
         self.cross.setIconSize(QSize(60, 60))  # Set the icon size
         self.cross.setStyleSheet("border: none;")  # Remove the button border
-        self.cross.move(370, 205)
+        self.cross.move(360, 205)
 
 
         self.square = QPushButton(self)
@@ -28,7 +28,7 @@ class ControllerDisplay(QWidget):
         self.square.setIcon(QIcon(f'{icons_path}/Button - PS Square White 1.png'))
         self.square.setIconSize(QSize(60, 60))  # Set the icon size
         self.square.setStyleSheet("border: none;")  # Remove the button border
-        self.square.move(320, 155)
+        self.square.move(310, 155)
 
 
         self.triangle = QPushButton(self)
@@ -36,65 +36,65 @@ class ControllerDisplay(QWidget):
         self.triangle.setIcon(QIcon(f'{icons_path}/Button - PS Triangle White 1.png'))
         self.triangle.setIconSize(QSize(60, 60))  # Set the icon size
         self.triangle.setStyleSheet("border: none;")  # Remove the button border
-        self.triangle.move(370, 105)
+        self.triangle.move(360, 105)
 
         self.arrow = QPushButton(self)
         self.arrow.setObjectName("arrow")
         self.arrow.setIcon(QIcon(f'{icons_path}/Button - PS Directional Arrows.png'))
         self.arrow.setIconSize(QSize(125, 125))  # Set the icon size
         self.arrow.setStyleSheet("border: none;")  # Remove the button border
-        self.arrow.move(20, 140)
+        self.arrow.move(10, 140)
 
         self.L1 = QPushButton(self)
         self.L1.setObjectName("L1")
         self.L1.setIcon(QIcon(f'{icons_path}/Button - PS L1.png'))
         self.L1.setIconSize(QSize(70, 70))  # Set the icon size
         self.L1.setStyleSheet("border: none;")  # Remove the button border
-        self.L1.move(50, 0)
+        self.L1.move(40, 0)
 
         self.L2 = QPushButton(self)
         self.L2.setObjectName("L2")
         self.L2.setIcon(QIcon(f'{icons_path}/Button - PS L2.png'))
         self.L2.setIconSize(QSize(70, 70))  # Set the icon size
         self.L2.setStyleSheet("border: none;")  # Remove the button border
-        self.L2.move(50, 40)
+        self.L2.move(40, 40)
 
         self.R1 = QPushButton(self)
         self.R1.setObjectName("R1")
         self.R1.setIcon(QIcon(f'{icons_path}/Button - PS R1.png'))
         self.R1.setIconSize(QSize(70, 70))  # Set the icon size
         self.R1.setStyleSheet("border: none;")  # Remove the button border
-        self.R1.move(370, 0)
+        self.R1.move(360, 0)
 
         self.R2 = QPushButton(self)
         self.R2.setObjectName("R2")
         self.R2.setIcon(QIcon(f'{icons_path}/Button - PS R2.png'))
         self.R2.setIconSize(QSize(70, 70))  # Set the icon size
         self.R2.setStyleSheet("border: none;")  # Remove the button border
-        self.R2.move(370, 40)
+        self.R2.move(360, 40)
 
         self.js_left = QPushButton(self)
         self.js_left.setObjectName("js_left")
         self.js_left.setIcon(QIcon(f'{icons_path}/Button - PS Analogue Blank.png'))
         self.js_left.setIconSize(QSize(70, 70))  # Set the icon size
         self.js_left.setStyleSheet("border: none;")  # Remove the button border
-        self.js_left.move(170, 230)
+        self.js_left.move(160, 220)
 
         self.js_right = QPushButton(self)
         self.js_right.setObjectName("js_right")
         self.js_right.setIcon(QIcon(f'{icons_path}/Button - PS Analogue Blank.png'))
         self.js_right.setIconSize(QSize(70, 70))  # Set the icon size
         self.js_right.setStyleSheet("border: none;")  # Remove the button border
-        self.js_right.move(270, 230)
+        self.js_right.move(260, 220)
 
         self.controller = controller
 
         # Update joystick status every 100 ms
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.update_joystick_status)
-        self.timer.start(100)
+        self.timer.timeout.connect(self.update)
+        self.timer.start(50)
 
-    def update_joystick_status(self):
+    def update(self):
         if not self.controller.connected:
             return
         states = self.controller.bindings_state
