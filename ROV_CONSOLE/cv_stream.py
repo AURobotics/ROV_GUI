aa2f2ebb908fe14acace33b9cba530a5dd67d280
Youssef Samy <office.youssefkassem@gmail.com>
2025-03-21 05:23:02 +0200

    RAII and moved measurement to a qt widget

--- ROV_CONSOLE/cv_stream.py
+++ ROV_CONSOLE/cv_stream.py
@@ -1,50 +1,52 @@
+"""
+    Fault-tolerant video stream handler using opencv.
+"""
+
 import cv2
 from threading import Thread
 from time import sleep
-from multiprocessing import Process
-from .measurement_temp import doit as measurement
 
 from os import path
 NO_VIDEO_INDICATOR = path.join(path.dirname(path.abspath(__file__)), 'assets', 'novideo.jpeg')
 
-class Camera:
-    def __init__(self, camera_fd):
-        self._cam = cv2.VideoCapture(camera_fd)
+class VideoStream:
+    def __init__(self, descriptor: int | str | None):
+        self._source = cv2.VideoCapture()
+        if descriptor is not None:
+            self._source.open(descriptor)
         self._no_frame = cv2.imread(NO_VIDEO_INDICATOR)
         self._frame = self._no_frame
         self._killswitch = False
-        self._frame_thread = Thread(target=self._frame_loop)
+        self._frame_thread = Thread(target=self._frame_loop, daemon=True)
         self._frame_thread.start()
-
-    def _frame_loop(self):
-        while not self._killswitch:
-            if self._cam.isOpened():
-                _, f = self._cam.read()
-                if _:
-                    self._frame = f
-            else:
-                self._frame = self._no_frame
-            sleep(0.015)
 
     @property
     def frame(self):
         return self._frame
 
-    def launch_length_measurement(self):
-        p = Process(target=measurement, args=(self._frame,))
-        p.start()
+    def _frame_loop(self):
+        try:
+            while not self._killswitch:
+                if self._source.isOpened():
+                    _, f = self._source.read()
+                    if _:
+                        self._frame = f
+                else:
+                    self._frame = self._no_frame
+                sleep(0.015)
+        except SystemExit:
+            self.kill()
 
-
-    def discard(self):
+    def kill(self):
         self._killswitch = True
         if self._frame_thread.is_alive():
             self._frame_thread.join()
-        if self._cam.isOpened():
-            self._cam.release()
+        if self._source.isOpened():
+            self._source.release()
 
     def __del__(self):
         self._killswitch = True
         if self._frame_thread.is_alive():
             self._frame_thread.join()
-        if self._cam.isOpened():
-            self._cam.release()
+        if self._source.isOpened():
+            self._source.release()
