271f49ec52b7948b7b3c026cff1283c9bb2b8f7c
Youssef Samy <office.youssefkassem@gmail.com>
2025-03-25 23:05:57 +0200

    - novideo is now loaded once - a much more fault tolerant resource management system

--- ROV_CONSOLE/cv_stream.py
+++ ROV_CONSOLE/cv_stream.py
@@ -3,183 +3,257 @@
 """
 from __future__ import annotations
 
-import enum
+from enum import Enum, auto
 from os import name as os_name
 from pathlib import Path
 from threading import Thread
 from time import sleep
+from typing import TypedDict, Optional
 
 import cv2
 import requests
 from cv2_enumerate_cameras import enumerate_cameras
 
 
-class ConnectionStatus(enum.Enum):
-    CONNECTED = enum.auto()
-    DISCONNECTED = enum.auto()
-    IN_PROGRESS = enum.auto()
-    MIRRORING = enum.auto()
+class ConnectionStatus(Enum):
+    CONNECTED = auto()
+    DISCONNECTED = auto()
+    IN_PROGRESS = auto()
+
+
+class DisconnectReason(Enum):
+    DESIRED_DISCONNECT = auto()
+    RESOURCE_BUSY = auto()
+    READ_ERROR = auto()
+    INVALID_DESCRIPTOR = auto()
+    RESOURCE_UNREACHABLE = auto()
+
+
+class CapType(str, Enum):
+    DEVICE = 'device'
+    IP = 'ip'
+    FILE = 'file'
+
+
+class CapMetadata(TypedDict):
+    type: Optional[CapType]
+    descriptor: Optional[str, int]
+    name: Optional[str]
+
+
+_ENUMERATION_APIS = {
+    'nt':    cv2.CAP_MSMF,
+    'posix': cv2.CAP_GSTREAMER
+    }
+_ENUM_API = _ENUMERATION_APIS[os_name]
 
 
 class VideoStream:
-    _ENUMERATION_APIS = {
-        'nt':    cv2.CAP_MSMF,
-        'posix': cv2.CAP_GSTREAMER
-        }
-    _ENUM_API = _ENUMERATION_APIS[os_name]
-    _EMPTY_FRAME = cv2.imread(str(Path(__file__).resolve().parent / 'assets' / 'novideo.jpeg'))
+    EMPTY_FRAME = cv2.imread(str(Path(__file__).resolve().parent / 'assets' / 'novideo.png'))
 
     _cap: cv2.VideoCapture
-    _cap_name: str | None
-    _cap_index: int | None
     _connection_status: ConnectionStatus
-    _cameras: dict[int:str]  # {index: name}
     _frame_thread: Thread
     _killswitch: bool
-    _upstream: VideoStream | None  # VideoStream to mirror while ConnectionStatus.MIRRORING
+    _downstreams: list[VideoStream]
+    _upstream: Optional[VideoStream]  # VideoStream to mirror while ConnectionStatus.MIRRORING
+    _cap_meta: CapMetadata
+    _disconnect_reason: Optional[DisconnectReason]
 
-    _pool: list[VideoStream] = []  # CLASS-ONLY ATTRIBUTE
+    # CLASS-ONLY ATTRIBUTES
+    _pool: list[VideoStream] = []
+    _connections_queue: list[(VideoStream, int | str | None, DisconnectReason | None)] = []
+    _disconnect_in_progress: bool = False
+    _connections_thread: Optional[Thread] = None
 
-    def __init__(self, descriptor: int | str | None = None):
+    def __init__(self, descriptor: Optional[int, str] = None):
         self._cap = cv2.VideoCapture()
-        self._cap_name = None
-        self._cap_index = None
+        self._cap_meta = {'type': None, 'descriptor': None, 'name': None}
         self._upstream = None
+        self._downstreams = []
         self._connection_status = ConnectionStatus.DISCONNECTED
         self.__class__._register_stream(self)
+        self._frame = None
+        self._killswitch = False
+        self._disconnect_reason = None
         if descriptor is not None:
             self.source = descriptor
-        self._frame = self._EMPTY_FRAME
-        self._killswitch = False
         self._frame_thread = Thread(target=self._frame_loop, daemon=True)
         self._frame_thread.start()
-        self._cameras = []
+
+    @classmethod
+    def _enqueue_connection(cls, obj: VideoStream, descriptor, disconnect_reason=DisconnectReason.DESIRED_DISCONNECT):
+        cls._connections_queue.append((obj, descriptor, disconnect_reason))
+        if cls._connections_thread is None or not cls._connections_thread.is_alive():
+            cls._connections_thread = Thread(target=cls._connections_manager, daemon=True)
+            cls._connections_thread.start()
+
+    @classmethod
+    def _connections_manager(cls):
+        while True:
+            if len(cls._connections_queue) > 0 and not cls._disconnect_in_progress:
+                vs, desc, reason = cls._connections_queue.pop(0)
+                if desc is not None:
+                    vs._connect(descriptor=desc)
+                else:
+                    vs._disconnect(reason=reason)
+            else:
+                sleep(0.01)
 
     @classmethod
     def _register_stream(cls, obj: VideoStream):
         cls._pool.append(obj)
 
     @classmethod
-    def mirror(cls, obj: VideoStream, descriptor) -> VideoStream | None:
+    def mirror(cls, obj: VideoStream, descriptor: int | str) -> Optional[VideoStream]:
         upstream = None
-        if type(descriptor) is int:
-            for s in cls._pool:
-                if s == obj or s._connection_status == ConnectionStatus.MIRRORING:
-                    continue
-                if s.source['index'] == descriptor:
-                    upstream = s
-                    break
+        cap_type = cls._resolve_cap_type(descriptor)
 
-        elif type(descriptor) is str:
-            path = Path(descriptor).resolve()
-            if path.exists(): descriptor = path.name
-            for s in cls._pool:
-                if s == obj or s._connection_status == ConnectionStatus.MIRRORING:
-                    continue
-                if s.source['name'] == descriptor:
-                    upstream = s
+        if cap_type == CapType.FILE:
+            descriptor = str(Path(descriptor).resolve())
+
+        for s in cls._pool:
+            if s == obj or s._upstream is not None:
+                continue
+            if s._cap_meta['descriptor'] == descriptor:
+                upstream = s
+
         return upstream
+
+    @staticmethod
+    def get_available_cameras():
+        return {cam.index: cam.name for cam in enumerate_cameras(_ENUM_API)}
 
     @property
     def available_cameras(self):
-        self._cameras = {cam.index: cam.name for cam in enumerate_cameras(self._ENUM_API)}
-        return self._cameras
+        return self.get_available_cameras()
 
     @property
     def connection_status(self):
         me = self
-        if self._connection_status == ConnectionStatus.MIRRORING:
+        if self._upstream is not None:
             me = self._upstream
         return me._connection_status
 
-    def _try_connect_to_url(self, url: str):
-        def url_reachable():
-            ok = False
-            try:
-                response = requests.get(url, timeout=5)
-                if response.status_code == 200:
-                    ok = True
-            except requests.RequestException:
-                ok = False
-            if not ok:
-                self._connection_status = ConnectionStatus.DISCONNECTED
-                self.source = None
-            else:
-                self._connection_status = ConnectionStatus.CONNECTED
-                self._cap_name = url
-                self._cap_index = None
-                self._cap.open(url)
+    @property
+    def disconnect_reason(self) -> DisconnectReason:
+        return self._disconnect_reason
 
-        connection_thread = Thread(target=url_reachable, daemon=True)
-        connection_thread.start()
+    @staticmethod
+    def _resolve_cap_type(descriptor: CapMetadata | int | str) -> Optional[CapType]:
+        if type(descriptor) is int:
+            if descriptor not in VideoStream.get_available_cameras():
+                return None
+            return CapType.DEVICE
+        elif type(descriptor) is str:
+            filepath = Path(descriptor).resolve()
+            if filepath.exists():
+                return CapType.FILE
+            else:
+                return CapType.IP
+        if type(descriptor) is CapMetadata:
+            return descriptor['type']
+        return None
 
     @property
     def source(self):
         me = self
-        if self._connection_status == ConnectionStatus.MIRRORING:
+        if self._upstream is not None:
             me = self._upstream
-        return {'index': me._cap_index, 'name': me._cap_name, 'connection_status': me._connection_status}
+        return me._cap_meta
 
     @source.setter
-    def source(self, descriptor: dict | int | str | None):
-
-        if self._connection_status == ConnectionStatus.IN_PROGRESS:
+    def source(self, descriptor: Optional[CapMetadata | int | str]):
+        if type(descriptor) is CapMetadata:
+            descriptor = descriptor['descriptor']
+        if descriptor == self._cap_meta['descriptor']:
             return
-
-        if self._connection_status == ConnectionStatus.MIRRORING:
-            self.source = None
-
+        if self._connection_status == ConnectionStatus.IN_PROGRESS: return
         self._connection_status = ConnectionStatus.IN_PROGRESS
+        self.__class__._enqueue_connection(self, descriptor)
 
-        if type(descriptor) is dict:
-            if 'index' in descriptor:
-                if descriptor['index'] is not None:
-                    descriptor = descriptor['index']
-            elif 'name' in descriptor:
-                descriptor = descriptor['name']
-            else:
-                descriptor = None
+    def _connect(self, descriptor: int | str):
+        cap_type = self._resolve_cap_type(descriptor)
 
-        if descriptor is None:
-            self._cap_index = None
-            self._cap_name = None
-            self._connection_status = ConnectionStatus.DISCONNECTED
-            self._cap.release()
+        if cap_type is None:
+            self._disconnect(reason=DisconnectReason.INVALID_DESCRIPTOR)
             return
-
         possible_upstream = self.__class__.mirror(self, descriptor)
         if possible_upstream is not None:
-            self._cap.release()
-            self._connection_status = ConnectionStatus.MIRRORING
+            self._disconnect(reason=DisconnectReason.DESIRED_DISCONNECT)
             self._upstream = possible_upstream
+            possible_upstream._downstreams.append(self)
             return
 
-        if type(descriptor) == int:
-            if descriptor not in self.available_cameras:
-                self.source = None
-                return
+        if self._upstream is not None or len(self._downstreams) > 0:
+            self._disconnect(reason=DisconnectReason.DESIRED_DISCONNECT)
+        if cap_type == CapType.DEVICE:
             ok = self._cap.open(descriptor)
             if not ok:
-                self.source = None
+                self._disconnect(reason=DisconnectReason.RESOURCE_BUSY)
                 return
-            self._cap_index = descriptor
-            self._cap_name = self.available_cameras[descriptor]
+            self._cap_meta['type'] = cap_type
+            self._cap_meta['descriptor'] = descriptor
+            self._cap_meta['name'] = self.available_cameras[descriptor]
             self._connection_status = ConnectionStatus.CONNECTED
 
-        elif type(descriptor) == str:
+        elif cap_type == CapType.FILE:
             filepath = Path(descriptor).resolve()
-            if filepath.exists():
-                ok = self._cap.open(str(filepath))
-                if not ok:
-                    self.source = None
-                    return
-                self._cap_index = None
-                self._cap_name = filepath.name
-                self._connection_status = ConnectionStatus.CONNECTED
+            ok = self._cap.open(str(filepath))
+            if not ok:
+                self._disconnect(reason=DisconnectReason.RESOURCE_BUSY)
+                return
+            self._cap_meta['type'] = cap_type
+            self._cap_meta['name'] = filepath.name
+            self._cap_meta['descriptor'] = str(filepath)
+            self._connection_status = ConnectionStatus.CONNECTED
+
+        elif cap_type == CapType.IP:
+            reachable = False
+            try:
+                response = requests.get(descriptor, timeout=5)
+                if response.status_code == 200:
+                    reachable = True
+            except requests.RequestException:
+                reachable = False
+            if not reachable:
+                self._disconnect(reason=DisconnectReason.RESOURCE_UNREACHABLE)
+                return
+            elif not self._cap.open(descriptor):
+                self._disconnect(reason=DisconnectReason.RESOURCE_BUSY)
             else:
-                self._try_connect_to_url(descriptor)
-        else:
-            self.source = None
+                self._connection_status = ConnectionStatus.CONNECTED
+                self._cap_meta['name'] = descriptor
+                self._cap_meta['descriptor'] = descriptor
+                self._cap_meta['type'] = cap_type
+
+    def _disconnect(self, reason: Optional[DisconnectReason], part_of_disconnect_chain=False) -> None:
+        if not part_of_disconnect_chain:
+            self.__class__._disconnect_in_progress = True
+        self._connection_status = ConnectionStatus.DISCONNECTED
+        while self._frame is not None:
+            sleep(0.15)
+        self._cap.release()
+        self._disconnect_reason = reason
+
+        if self._upstream is not None:
+            self._upstream._downstreams.remove(self)
+            self._upstream = None
+
+        descriptor = self._cap_meta['descriptor']
+        self._cap_meta['descriptor'] = None
+        self._cap_meta['type'] = None
+        self._cap_meta['name'] = None
+
+        if len(self._downstreams) > 0:
+            downstreams = self._downstreams.copy()
+            for d in downstreams:
+                d._disconnect(reason=DisconnectReason.DESIRED_DISCONNECT, part_of_disconnect_chain=True)
+            if reason == DisconnectReason.DESIRED_DISCONNECT:
+                for d in downstreams:
+                    d.source = descriptor
+        if not part_of_disconnect_chain:
+            self.__class__._disconnect_in_progress = False
 
     @property
     def frame(self):
@@ -189,17 +263,19 @@ class VideoStream:
         try:
             while not self._killswitch:
                 sleep(0.015)
-                if self._connection_status == ConnectionStatus.MIRRORING:
+                if self._upstream is not None:
                     self._frame = self._upstream._frame
                     continue
-                if self._cap.isOpened() and self._connection_status == ConnectionStatus.CONNECTED:
+                if self._connection_status == ConnectionStatus.CONNECTED:
                     ok, f = self._cap.read()
-                    if ok:
-                        self._frame = f
+                    if not self._cap.isOpened() or not ok:
+                        self._connection_status = ConnectionStatus.DISCONNECTED
+                        self.__class__._enqueue_connection(self, None, DisconnectReason.RESOURCE_BUSY)
+                        self._frame = None
                         continue
-                    else:
-                        self.source = None
-                self._frame = self._EMPTY_FRAME
+                    self._frame = f
+                    continue
+                self._frame = None
         except SystemExit:
             self.kill()
 
