eefbb8aeb7cb9768da29114f96bd8648454fffd9
Youssef Samy <office.youssefkassem@gmail.com>
2025-03-25 23:06:29 +0200

    - fixed memory leak from menubar - preparing for gui layout overhaul

--- ROV_CONSOLE/gui.py
+++ ROV_CONSOLE/gui.py
@@ -1,21 +1,18 @@
 import random
 from functools import partial
+from pathlib import Path
 
-import requests
 from PySide6.QtCore import QTimer, Qt, QSize
-from PySide6.QtGui import QImage, QPixmap, QPainter, QColor, QPen, QBrush, QIcon
+from PySide6.QtGui import QImage, QPixmap, QPainter, QColor, QPen, QBrush, QIcon, QAction
 from PySide6.QtWidgets import (
     QMainWindow,
     QLabel,
     QWidget,
     QGridLayout,
     QVBoxLayout,
-    QScrollArea,
-    QCheckBox,
     QPushButton,
-    QMenu,
     QInputDialog,
-    QLineEdit,
+    QLineEdit, QMenuBar, QMenu, QToolButton,
     )
 
 from .controller_widget import ControllerDisplay
@@ -24,23 +21,24 @@ from .esp32 import ESP32
 from .gamepad import Controller
 from .measurement_widget import MeasurementWindow
 
-RASPBERY_PI_IP = "192.168.1.2"
-
-from pathlib import Path
-
-ICONS_PATH = Path(__file__).resolve().parent / 'assets'
+ASSETS_PATH = Path(__file__).resolve().parent / 'assets'
 camera_toolbar_icons = {
-    "hflip":       QIcon(str(ICONS_PATH / "flip-horizontal.svg")),
-    "vflip":       QIcon(str(ICONS_PATH / "flip-vertical.svg")),
-    "measurement": QIcon(str(ICONS_PATH / "ruler.svg")),
-    "pano":        QIcon(str(ICONS_PATH / "pano.svg")),
+    'hflip':       QIcon(str(ASSETS_PATH / 'flip-horizontal.svg')),
+    'vflip':       QIcon(str(ASSETS_PATH / 'flip-vertical.svg')),
+    'measurement': QIcon(str(ASSETS_PATH / 'ruler.svg')),
+    'pano':        QIcon(str(ASSETS_PATH / 'pano.svg'))
     }
+APP_ICON = ASSETS_PATH / 'appicon.png'
 
 
-class CameraWidget(QLabel):
+class CameraWidget(QWidget):
+
     def __init__(self, parent, cam):
         super().__init__(parent)
         self._stream = VideoStream(cam)
+        self._view = QLabel(self)
+        self._view.setScaledContents(True)
+        self._empty_frame = self._pixmap_from_frame(self._stream.EMPTY_FRAME)
         self.setAttribute(Qt.WidgetAttribute.WA_Hover)
         self.bottom_buttons = {}
         for b in camera_toolbar_icons:
@@ -51,13 +49,21 @@ class CameraWidget(QLabel):
             self.bottom_buttons[b] = pb
         self.h_mirror = False
         self.v_mirror = False
-        self.bottom_buttons["hflip"].clicked.connect(self.hflip)
-        self.bottom_buttons["vflip"].clicked.connect(self.vflip)
-        self.bottom_buttons["measurement"].clicked.connect(
+        self.bottom_buttons['hflip'].clicked.connect(self.hflip)
+        self.bottom_buttons['vflip'].clicked.connect(self.vflip)
+        self.bottom_buttons['measurement'].clicked.connect(
             self._launch_length_measurement
             )
-
-        self.measurement_window: QWidget | None = None
+        self.camera_choice_button = QToolButton(self)
+        self.camera_choice_button.setText('Cameras')
+        self.camera_choice_menu = QMenu(self)
+        self.camera_choice_button.setPopupMode(QToolButton.ToolButtonPopupMode.InstantPopup)
+        self.camera_choice_button.setMenu(self.camera_choice_menu)
+        cameras = self._stream.available_cameras
+        for c in cameras:
+            action = QAction(f'{c}:{cameras[c]}', self)
+            self.camera_choice_menu.addAction(action)
+            action.triggered.connect(partial(self.change_cam, int(c)))
 
     def hflip(self):
         self.h_mirror = not self.h_mirror
@@ -72,14 +78,20 @@ class CameraWidget(QLabel):
             b.move(w, h)
             b.setVisible(True)
             b.raise_()
-            w += 48
+            w += 36
 
     def leaveEvent(self, event):
         for b in self.bottom_buttons.values():
             b.setVisible(False)
 
-    def _pixmap_from_frame(self):
+    def _pixmap_from_frame(self, frame):
+        return QPixmap(
+            QImage(frame.data, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format.Format_BGR888))
+
+    def _pixmap_from_stream(self):
         frame = self._stream.frame
+        if frame is None:
+            return self._empty_frame
         q_image = (
             QImage(
                 frame.data,
@@ -88,23 +100,26 @@ class CameraWidget(QLabel):
                 frame.strides[0],
                 QImage.Format.Format_BGR888,
                 )
-            .smoothScaled(self.width(), self.height())
             .mirrored(horizontally=self.h_mirror, vertically=self.v_mirror)
         )
         return QPixmap.fromImage(q_image)
 
     def _launch_length_measurement(self):
-        self.measurement_window = MeasurementWindow(self, self._pixmap_from_frame())
+        self.measurement_window = MeasurementWindow(self, self._pixmap_from_stream())
+
+    def resizeEvent(self, event):
+        self._view.resize(event.size())
+
+    def change_cam(self, cam):
+        self._stream.source = cam
 
     def update(self):
-        self.setPixmap(self._pixmap_from_frame())
+        self._view.setPixmap(self._pixmap_from_stream())
 
 
 class OrientationsWidget(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
-
-        self.setMinimumSize(parent.width() // 3, parent.height() // 2)
 
         layout = QVBoxLayout()
 
@@ -128,18 +143,16 @@ class OrientationsWidget(QWidget):
         pitchReading = random.randint(0, 10)
         rollReading = random.randint(0, 10)
 
-        self.depthLabel.setText("Depth: " + str(depthReading))
-        self.yawLabel.setText("Yaw: " + str(yawReading))
-        self.pitchLabel.setText("Pitch: " + str(pitchReading))
-        self.rollLabel.setText("Roll: " + str(rollReading))
+        self.depthLabel.setText('Depth: ' + str(depthReading))
+        self.yawLabel.setText('Yaw: ' + str(yawReading))
+        self.pitchLabel.setText('Pitch: ' + str(pitchReading))
+        self.rollLabel.setText('Roll: ' + str(rollReading))
 
 
 class ThrustersWidget(QWidget):
     def __init__(self, parent):
         super().__init__(parent)
         self.parent = parent
-
-        self.setMinimumSize(parent.width() // 3, parent.height() // 4)
 
         self.frontLabel = QLabel(self)
         self.backLabel = QLabel(self)
@@ -167,7 +180,7 @@ class ThrustersWidget(QWidget):
             ]
 
     def set_colors(self):
-        """Allows changing colors dynamically"""
+        '''Allows changing colors dynamically'''
 
         frontRightSpeed = 0
         backRightSpeed = 255
@@ -289,14 +302,157 @@ class ThrustersWidget(QWidget):
         self.update()
 
 
+class MenuBar(QMenuBar):
+    _esp_menu: QMenu
+    _controller_menu: QMenu
+    _esp_actions: list[QAction]
+
+    def __init__(self, parent, esp: ESP32, controller: Controller):
+        super().__init__(parent)
+        self._esp = esp
+        self._esp_menu = self.addMenu('ESP')
+        self._esp_menu_sep = self._esp_menu.addSeparator()
+        self._esp_menu_add_custom = self._esp_menu.addAction('Custom Port')
+        self._esp_menu_add_custom.triggered.connect(self.manual_port_selection)
+        self._esp_menu_reset_esp = QAction('Reset ESP')
+        self._esp_menu_reset_esp.triggered.connect(self._esp.reset)
+        self._esp_menu_no_esp = QAction('No Serial Ports')
+        self._esp_menu_no_esp.setEnabled(False)
+        self._displayed_ports: list[QAction] = []
+        self._port_slots: dict[str:partial] = {}
+        self._stored_ports = []
+        self._stored_custom_port = None
+        self._controller = controller
+        self._controller_menu = self.addMenu('Controller')
+        self._displayed_controllers: list[QAction] = []
+        self._stored_controllers: list[str] = []
+        self._gp_menu_none_connected = QAction('No ControllerS Connected')
+        self._gp_menu_none_connected.setEnabled(False)
+        self._gp_slots: dict[str:partial] = {}
+
+    def update(self):
+        self._update_controller_menu()
+        self._update_esp_menu()
+
+    def _update_controller_menu(self):
+        gamepads = self._controller.gamepads
+        chosen = self._controller.gamepad
+        new_gamepads = [gp for gp in gamepads if gp not in self._stored_controllers]
+        removed_gamepads = [gp for gp in self._stored_controllers if gp not in gamepads]
+        if len(gamepads) == 0:
+            if len(removed_gamepads) != 0:
+                self._controller_menu.addAction(self._gp_menu_none_connected)
+        else:
+            self._controller_menu.removeAction(self._gp_menu_none_connected)
+        for gp in new_gamepads:
+            option = QAction(f'{gp}')
+            self._controller_menu.addAction(option)
+            f = partial(self.toggle_controller, gp)
+            self._gp_slots.update({gp: f})
+            option.triggered.connect(f)
+            self._displayed_controllers.append(option)
+        for gp in removed_gamepads:
+            for option in self._displayed_controllers:
+                if option.text() == gp:
+                    f = self._gp_slots.pop(gp)
+                    option.triggered.disconnect(f)
+                    self._controller_menu.removeAction(option)
+                    self._displayed_controllers.remove(option)
+        self._stored_controllers = gamepads
+        for option in self._displayed_controllers:
+            if chosen == option.text():
+                option.setCheckable(True)
+                option.setChecked(True)
+            else:
+                option.setChecked(False)
+                option.setCheckable(False)
+
+    def _update_esp_menu(self):
+        # Tracking changes
+        ports = self._esp.available_ports
+        chosen = self._esp.port
+        new_custom_port = chosen if chosen not in ports and chosen != self._stored_custom_port else None
+        new_ports = [p for p in ports if p not in self._stored_ports]
+        removed_ports = [p for p in self._stored_ports if p not in ports]
+        if len(ports) == 0:
+            if len(removed_ports) != 0:
+                self._esp_menu.insertAction(self._esp_menu_sep, self._esp_menu_no_esp)
+        else:
+            self._esp_menu.removeAction(self._esp_menu_no_esp)
+        for port in new_ports:
+            option = QAction(f'{port}')
+            self._esp_menu.insertAction(self._esp_menu_sep, option)
+            f = partial(self.toggle_port, port)
+            self._port_slots.update({port: f})
+            option.triggered.connect(f)
+            self._displayed_ports.append(option)
+        if chosen is None and self._stored_custom_port is not None and new_custom_port is None:
+            removed_ports.append(self._stored_custom_port)
+        for port in removed_ports:
+            for option in self._displayed_ports:
+                if option.text() == port:
+                    f = self._port_slots.pop(port)
+                    option.triggered.disconnect(f)
+                    self._esp_menu.removeAction(option)
+                    self._displayed_ports.remove(option)
+        self._stored_ports = ports
+        if new_custom_port is not None:
+            self._stored_custom_port = new_custom_port
+            option = self._esp_menu.addAction(f'{new_custom_port}')
+            f = partial(self.toggle_port, new_custom_port)
+            self._port_slots.update({new_custom_port: f})
+            option.triggered.connect(f)
+            self._displayed_ports.append(option)
+
+        for option in self._displayed_ports:
+            if chosen == option.text():
+                option.setCheckable(True)
+                option.setChecked(True)
+            else:
+                option.setChecked(False)
+                option.setCheckable(False)
+        if chosen is not None:
+            if new_custom_port is not None or len(new_ports) != 0:
+                self._esp_menu.addAction(self._esp_menu_reset_esp)
+        else:
+            self._esp_menu.removeAction(self._esp_menu_reset_esp)
+
+    def toggle_port(self, port):
+        if self._esp.port == port:
+            self._esp.disconnect()
+            self._controller.payload_callback = None
+        else:
+            self._esp.connect(port)
+            self._controller.payload_callback = self._esp.send
+
+    def manual_port_selection(self):
+        text, ok = QInputDialog.getText(
+            self,
+            'Custom Port',
+            'Port Name (RFC2217 NOT FULLY SUPPORTED):',
+            QLineEdit.EchoMode.Normal,
+            'COM',
+            )
+        if ok:
+            self.toggle_port(text)
+
+    def toggle_controller(self, indexed_name):
+        if self._controller.connected:
+            if indexed_name == self._controller.gamepad:
+                self._controller.gamepad = None
+                return
+        i = indexed_name[: indexed_name.find(':')]
+        self._controller.gamepad = int(i)
+
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.showMaximized()
-        self.setWindowTitle("AU Robotics ROV GUI")
+        self.setWindowTitle('AU Robotics ROV GUI')
+        self.setWindowIcon(QIcon(str(APP_ICON)))
 
-        self.state = self.windowState()
         self.controller = Controller()
         self.esp = ESP32()
         self.initUI()
@@ -309,147 +465,46 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        def is_url_reachable(url):
-            try:
-                response = requests.get(url, timeout=5)
-                return response.status_code == 200
-            except requests.RequestException:
-                return False
-
         self.leftCameraWidget = CameraWidget(self, 0)
-        self.middleCameraWidget = CameraWidget(self, 1)
+        self.middleCameraWidget = CameraWidget(self, 0)
         self.rightCameraWidget = CameraWidget(self, 2)
         self.orientationsWidget = OrientationsWidget(self)
         self.controllerWidget = ControllerDisplay(self.controller)
         self.thrustersWidget = ThrustersWidget(self)
-        self.tasksWidget = QScrollArea(self)
 
-        self.menu_bar = self.menuBar()
-        self.initTasks()
+        self.menu_bar = MenuBar(self, self.esp, self.controller)
+        self.setMenuBar(self.menu_bar)
 
         grid = QGridLayout()
-
-        # grid.setColumnMinimumWidth(0, self.width() // 3)
-        # grid.setColumnMinimumWidth(1, self.width() // 3)
-        # grid.setColumnMinimumWidth(2, self.width() // 3)
-
-        # grid.setRowMinimumHeight(0, self.height() // 4)
-        # grid.setRowMinimumHeight(1, self.height() // 4)
-        # grid.setRowMinimumHeight(2, self.height() // 4)
-        # grid.setRowMinimumHeight(3, self.height() // 4)
 
         grid.setColumnStretch(0, 1)
         grid.setColumnStretch(1, 1)
         grid.setColumnStretch(2, 1)
+        grid.setColumnStretch(3, 1)
 
         grid.setRowStretch(0, 1)
         grid.setRowStretch(1, 1)
         grid.setRowStretch(2, 1)
-        grid.setRowStretch(3, 2)
+        grid.setRowStretch(3, 1)
 
-        grid.addWidget(self.leftCameraWidget, 0, 0, 2, 1)
-        grid.addWidget(self.middleCameraWidget, 0, 1, 2, 1)
-        grid.addWidget(self.rightCameraWidget, 0, 2, 2, 1)
+        grid.addWidget(self.leftCameraWidget, 0, 0, 2, 3)
+        grid.addWidget(self.middleCameraWidget, 0, 3, 1, 1)
+        grid.addWidget(self.rightCameraWidget, 1, 3, 1, 1)
 
         grid.addWidget(self.orientationsWidget, 2, 0, 2, 1)
 
-        grid.addWidget(self.tasksWidget, 2, 1, 1, 1)
-        grid.addWidget(self.controllerWidget, 3, 1, 1, 1)
-        grid.addWidget(self.thrustersWidget, 3, 2, 1, 1)
+        grid.addWidget(self.controllerWidget, 2, 1, 2, 2)
+        grid.addWidget(self.thrustersWidget, 2, 2, 2, 2)
 
         central_widget.setLayout(grid)
 
-    def createMenuBar(self):
-        self.menu_bar.clear()
-        port_menu = QMenu("Serial Port", self)
-        port_is_from_choices = False
-        self.menu_bar.addMenu(port_menu)
-        for i in self.esp.available_ports:
-            port_sel = port_menu.addAction(f"{i}")
-            port_sel.setCheckable(True)
-            if i == self.esp.port:
-                port_sel.setChecked(True)
-                port_is_from_choices = True
-            port_sel.triggered.connect(partial(self.toggle_port, i))
-        if self.esp.port is not None and not port_is_from_choices:
-            port_sel = port_menu.addAction(f"{self.esp.port}")
-            port_sel.setCheckable(True)
-            port_sel.setChecked(True)
-            port_sel.triggered.connect(partial(self.toggle_port, self.esp.port))
-        port_menu.addSeparator()
-        manual_port_selection = port_menu.addAction("Custom Port Selection")
-        manual_port_selection.triggered.connect(partial(self.manual_port_selection))
-        if self.esp.connected:
-            reset_esp = port_menu.addAction("Reset ESP")
-            reset_esp.triggered.connect(self.esp.reset)
-
-        if not self.controller.gamepads:
-            return
-        controller_menu = QMenu("Controller", self)
-        self.menu_bar.addMenu(controller_menu)
-        for gp in self.controller.gamepads:
-            gp_sel = controller_menu.addAction(f"{gp}")
-            gp_sel.triggered.connect(partial(self.toggle_controller, gp))
-            gp_sel.setCheckable(True)
-            if self.controller.gamepad == gp:
-                gp_sel.setChecked(True)
-
-    def manual_port_selection(self):
-        text, ok = QInputDialog.getText(
-            self,
-            "Custom Port",
-            "Port Name (RFC2217 NOT FULLY SUPPORTED):",
-            QLineEdit.EchoMode.Normal,
-            "COM",
-            )
-        if ok:
-            self.toggle_port(text)
-
-    def toggle_port(self, port):
-        if self.esp.port == port:
-            self.esp.disconnect()
-            self.controller.payload_callback = None
-        else:
-            self.esp.connect(port)
-            self.controller.payload_callback = self.esp.send
-
-    def toggle_controller(self, indexed_name):
-        if self.controller.connected:
-            if indexed_name == self.controller.gamepad:
-                self.controller.gamepad = None
-                return
-        i = indexed_name[: indexed_name.find(":")]
-        self.controller.gamepad = int(i)
-
     def updateFrame(self):
-        if self.state != self.windowState():
-            self.state = self.windowState()
         self.orientationsWidget.update()
         self.thrustersWidget.updateThrusters()
-        self.createMenuBar()
         self.leftCameraWidget.update()
         self.middleCameraWidget.update()
         self.rightCameraWidget.update()
+        self.menu_bar.update()
         if self.esp.connected:
             while self.esp.incoming:
                 print(self.esp.next_line)
-
-    def initTasks(self):
-        tasksContainer = QWidget()
-        tasksScrollLayout = QVBoxLayout(tasksContainer)
-
-        tasksScrollLayout.addWidget(QCheckBox("Task 1"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 2"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 3"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 4"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 5"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 6"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 7"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 8"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 9"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 10"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 11"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 12"))
-        tasksScrollLayout.addWidget(QCheckBox("Task 13"))
-
-        self.tasksWidget.setWidget(tasksContainer)
