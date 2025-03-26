a2fdec060173d99be5da27e867d4e46946491662
Youssef Samy <office.youssefkassem@gmail.com>
2025-03-26 21:31:01 +0200

    change all asset path resolutions to be constant at best, or relative to a constant at worst

--- ROV_CONSOLE/controller_widget.py
+++ ROV_CONSOLE/controller_widget.py
@@ -1,10 +1,11 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QLabel
 from PySide6.QtCore import QTimer, QSize
 from PySide6.QtGui import QIcon
-import os
-_ = os.path.dirname(os.path.abspath(__file__))
-ICONS_PATH = os.path.join(_, 'assets', 'ds4icons')
-DS4_ICONS = {f[:f.find('.')]:QIcon(os.path.join(ICONS_PATH, f)) for f in os.listdir(ICONS_PATH) if os.path.isfile(os.path.join(ICONS_PATH, f))}
+from PySide6.QtWidgets import QWidget, QPushButton, QLabel
+
+from .constants import DS4_ICONS_PATHS
+
+DS4_ICONS = {f.stem: QIcon(str(f)) for f in DS4_ICONS_PATHS}
+
 
 class ControllerDisplay(QWidget):
     def __init__(self, controller):
@@ -12,125 +13,125 @@ class ControllerDisplay(QWidget):
 
         self.button_scheme = {
             'CIRCLE': {
-                'size': QSize(50, 50),
-                'icons': ('CIRCLE', 'CIRCLE-1'),
+                'size':     QSize(50, 50),
+                'icons':    ('CIRCLE', 'CIRCLE-1'),
                 'position': (410, 145)
-            },
+                },
             'CROSS':
-                {
-                    'size': QSize(50, 50),
-                    'icons': ('CROSS', 'CROSS-1'),
-                    'position': (370, 185)
-                },
+                      {
+                          'size':     QSize(50, 50),
+                          'icons':    ('CROSS', 'CROSS-1'),
+                          'position': (370, 185)
+                          },
             'SQUARE':
-                {
-                    'size': QSize(50, 50),
-                    'icons': ('SQUARE', 'SQUARE-1'),
-                    'position': (330, 145)
-                },
+                      {
+                          'size':     QSize(50, 50),
+                          'icons':    ('SQUARE', 'SQUARE-1'),
+                          'position': (330, 145)
+                          },
             'TRIANGLE':
-                {
-                    'size': QSize(50, 50),
-                    'icons': ('TRIANGLE', 'TRIANGLE-1'),
-                    'position': (370, 105)
-                },
+                      {
+                          'size':     QSize(50, 50),
+                          'icons':    ('TRIANGLE', 'TRIANGLE-1'),
+                          'position': (370, 105)
+                          },
             'D-UP':
-                {
-                    'size': QSize(50, 50),
-                    'icons': ('D-UP', 'D-UP-1'),
-                    'position': (40, 105)
-                },
+                      {
+                          'size':     QSize(50, 50),
+                          'icons':    ('D-UP', 'D-UP-1'),
+                          'position': (40, 105)
+                          },
             'D-DOWN':
-                {
-                    'size': QSize(50, 50),
-                    'icons': ('D-DOWN', 'D-DOWN-1'),
-                    'position': (40, 185)
-                },
+                      {
+                          'size':     QSize(50, 50),
+                          'icons':    ('D-DOWN', 'D-DOWN-1'),
+                          'position': (40, 185)
+                          },
             'D-LEFT':
-                {
-                    'size': QSize(50, 50),
-                    'icons': ('D-LEFT', 'D-LEFT-1'),
-                    'position': (0, 145)
-                },
+                      {
+                          'size':     QSize(50, 50),
+                          'icons':    ('D-LEFT', 'D-LEFT-1'),
+                          'position': (0, 145)
+                          },
             'D-RIGHT':
-                {
-                    'size': QSize(50, 50),
-                    'icons': ('D-RIGHT', 'D-RIGHT-1'),
-                    'position': (80, 145)
-                },
+                      {
+                          'size':     QSize(50, 50),
+                          'icons':    ('D-RIGHT', 'D-RIGHT-1'),
+                          'position': (80, 145)
+                          },
             'L1':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('L1', 'L1-1'),
-                    'position': (0,40)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('L1', 'L1-1'),
+                          'position': (0, 40)
+                          },
             'L2':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('L2', 'L2-1'),
-                    'position': (0,0)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('L2', 'L2-1'),
+                          'position': (0, 0)
+                          },
             'R1':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('R1', 'R1-1'),
-                    'position': (410, 40)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('R1', 'R1-1'),
+                          'position': (410, 40)
+                          },
             'R2':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('R2', 'R2-1'),
-                    'position': (410, 0)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('R2', 'R2-1'),
+                          'position': (410, 0)
+                          },
             'LS':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('STICK-BASE', 'STICK-BASE'),
-                    'position': (145, 190)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('STICK-BASE', 'STICK-BASE'),
+                          'position': (145, 190)
+                          },
             'RS':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('STICK-BASE', 'STICK-BASE'),
-                    'position': (265, 190)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('STICK-BASE', 'STICK-BASE'),
+                          'position': (265, 190)
+                          },
             'L3':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('LS', 'L3'),
-                    'position': (145, 190)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('LS', 'L3'),
+                          'position': (145, 190)
+                          },
             'R3':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('RS', 'R3'),
-                    'position': (265, 190)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('RS', 'R3'),
+                          'position': (265, 190)
+                          },
             'PS':
-                {
-                    'size': QSize(30,30),
-                    'icons': ('PS', 'PS-1'),
-                    'position': (225, 230)
-                },
+                      {
+                          'size':     QSize(30, 30),
+                          'icons':    ('PS', 'PS-1'),
+                          'position': (225, 230)
+                          },
             'TOUCHPAD':
-                {
-                    'size': QSize(300,150),
-                    'icons': ('TOUCHPAD', 'TOUCHPAD-1'),
-                    'position': (85, 0)
-                },
+                      {
+                          'size':     QSize(300, 150),
+                          'icons':    ('TOUCHPAD', 'TOUCHPAD-1'),
+                          'position': (85, 0)
+                          },
             'SHARE':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('SHARE', 'SHARE-1'),
-                    'position': (100, 0)
-                },
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('SHARE', 'SHARE-1'),
+                          'position': (100, 0)
+                          },
             'OPTIONS':
-                {
-                    'size': QSize(70,70),
-                    'icons': ('OPTIONS', 'OPTIONS-1'),
-                    'position': (300, 0)
-                }
-        }
+                      {
+                          'size':     QSize(70, 70),
+                          'icons':    ('OPTIONS', 'OPTIONS-1'),
+                          'position': (300, 0)
+                          }
+            }
 
         self.buttons = {}
         for b in self.button_scheme:
@@ -144,7 +145,7 @@ class ControllerDisplay(QWidget):
             self.buttons[b] = button
 
         self.no_controller_label = QLabel('Please connect a controller.', self)
-        self.no_controller_label.move(205,105)
+        self.no_controller_label.move(205, 105)
         self.no_controller_label.setVisible(False)
 
         self.controller = controller
